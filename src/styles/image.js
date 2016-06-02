94b235b3f9d3db8d0ac075b96cdfd4cf8b52922d
Xiaobo <xiaobo@uvoplus.com>
2016-06-02 19:45:00 +0800

    圆形图片定义

--- src/styles/image.js
+++ src/styles/image.js
@@ -13,6 +13,21 @@ import {
 import {IMAGE_SIZE} from './variable';
 
 export const UjlImage = StyleSheet.create({
+  avatar: {
+    width: IMAGE_SIZE.BASE,
+    height: IMAGE_SIZE.BASE,
+    borderRadius: IMAGE_SIZE.BASE/2,
+  },
+  avatarsm: {
+    width: IMAGE_SIZE.SM,
+    height: IMAGE_SIZE.SM,
+    borderRadius: IMAGE_SIZE.SM/2,
+  },
+  avatarlg: {
+    width: IMAGE_SIZE.LG,
+    height: IMAGE_SIZE.LG,
+    borderRadius: IMAGE_SIZE.LG/2,
+  },
   base: {
     width: IMAGE_SIZE.BASE,
     height: IMAGE_SIZE.BASE,
